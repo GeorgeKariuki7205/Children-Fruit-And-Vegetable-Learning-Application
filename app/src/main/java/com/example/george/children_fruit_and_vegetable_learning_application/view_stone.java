5922270ffd3c5334b4b51799d0e6e7af63b31d31
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-07-12 15:53:32 +0300

    Finished on all the activities Involved.

--- app/src/main/java/com/example/george/children_fruit_and_vegetable_learning_application/view_stone.java
+++ app/src/main/java/com/example/george/children_fruit_and_vegetable_learning_application/view_stone.java
@@ -1,9 +1,7 @@
 package com.example.george.children_fruit_and_vegetable_learning_application;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 public class view_stone extends AppCompatActivity {
 
@@ -11,16 +9,5 @@ public class view_stone extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_stone);
-
-        //Adding the tool bar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setSubtitle("Stone Fruits");
-
-        //Adding the back naviation
-        //ADDING THE BACK NAVIGATION.
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/george/children_fruit_and_vegetable_learning_application/view_tropical_fruits.java
+++ app/src/main/java/com/example/george/children_fruit_and_vegetable_learning_application/view_tropical_fruits.java
@@ -1,9 +1,7 @@
 package com.example.george.children_fruit_and_vegetable_learning_application;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 public class view_tropical_fruits extends AppCompatActivity {
 
@@ -11,15 +9,5 @@ public class view_tropical_fruits extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_tropical_fruits);
-        //Adding the tool bar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setSubtitle("Tropical Fruits");
-
-        //Adding the back naviation
-        //ADDING THE BACK NAVIGATION.
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
